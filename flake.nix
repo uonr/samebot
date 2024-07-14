18c825dc136ea69467345595d228f5d7fc09b8fb
uonr <dev@yuru.me>
2024-07-14 18:09:45 +0900

    feat: flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A birdge between Cytube and Telegram.";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { buildInputs = with pkgs; [ nodejs ]; };
+      }
+    );
+}
